c142ccebb7daa226684a79dce9294368014d7010
LauMimi <MimiLau@LauMimis-iMac.local>
2015-07-03 18:12:12 +0800

    Mimi 20150703 6:12pm

--- wp-content/themes/egg/assets/js/_main.js
+++ wp-content/themes/egg/assets/js/_main.js
@@ -34,8 +34,44 @@ var Roots = {
 	  $('#main-banner').slick({
 		infinite: true,
 		autoplay: true,
-  autoplaySpeed: 2000
+		autoplaySpeed: 2000
 	  });
+	  
+	  $('.home-news .section-contents').slick({
+		  infinite: true,
+		  speed: 300,
+		  slidesToShow: 3,
+		  slidesToScroll: 3,
+		  responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		  ]
+	  });
+
+
 	});
 	  
     }
--- wp-content/themes/egg/templates/content-home.php
+++ wp-content/themes/egg/templates/content-home.php
@@ -27,8 +27,31 @@
 <section class="home-news">
 <div class="container">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 sectionIntro">
-		<h2>News</h2>
-		<div class="section-contents"><p>contents goes here.</p></div>
+		<div class="section-title"><h2>News</h2></div> 
+		<div class="section-contents">
+        	<?
+	    	$args= array(
+			    'post_type' => 'post',
+			    'post_status' 		=> 'publish',
+			    'orderby'			=> 'date',
+			    'order' 			=> 'DESC',
+			    'numberposts' 		=> -1,
+			    'suppress_filters' 	=> 0
+			  );
+	    	$results = get_posts( $args );
+	    	foreach( $results as $result ){
+	    		$url = wp_get_attachment_url( get_post_thumbnail_id($result->ID, 'thumbnail',array('class'=> "media-object")) );
+				$post_title = get_the_title( $result->ID );
+				
+				echo '<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">';
+				echo '<div class="news-slider">';
+					echo '<div class="post-img"><a href="'.get_permalink($result->ID).'"><img src="'. $url.'" /></a></div>';
+					echo '<div class="post-title"><a href="'.get_permalink($result->ID).'">'.$post_title.'</a></div>';
+				echo '</div></div>';
+				
+            }
+			?>
+        </div>
 	</div>
 </div>
 </section>
@@ -36,7 +59,7 @@
 <section class="home-new-collection">
 <div class="container">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 sectionIntro">
-		<h2>New Collection</h2>
+		<div class="section-title"><h2>New Collection</h2></div>
 		<div class="section-contents">contents goes here.</div>
 	</div>
 </div>
@@ -45,7 +68,7 @@
 <section class="home-collections">
 <div class="container">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 sectionIntro">
-		<h2>Collections</h2>
+		<div class="section-title"><h2>Collections</h2></div>
 		<div class="section-contents">contents goes here.</div>
 	</div>
 </div>
@@ -54,7 +77,12 @@
 <section class="home-find-store">
 <div class="container">
 	<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 sectionIntro">
-		<h2>Find a EGG Store</h2>
+		<div class="section-title">
+       	  <a href="#">
+          	<div class="find-store-btn"><h2>Find a EGG Store</h2></div>
+          	<img class="find-store-arrow" src="<?=get_stylesheet_directory_uri()?>/assets/img/arrow-right-brown.png"  />
+          </a>
+        </div>
 		
 	</div>
 </div>
